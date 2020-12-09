734680d30d93a6eb21bc91039797b0d179f41e57
Eugene Teoh <eugenetwc1@gmail.com>
2020-12-09 18:18:02 +0000

    Updates + implemented GF(2^4) addition and multiplication

---
+++ MiniAES.cpp
@@ -0,0 +1,95 @@
+#include "MiniAES.h"
+#include <iostream>
+#include <string>
+#include <array>
+#include <unordered_map>
+
+uint8_t gadd(uint8_t a, uint8_t b) {
+    // GF(2^4) addition
+    return (a ^ b);
+}
+
+uint8_t gmul(uint8_t a, uint8_t b) {
+    // GF(2^4) multiplication
+    // using modified version of russian peasant algorithm
+    // it also uses branchless programming to prevent timing attacks
+    uint8_t p = 0;
+    for (auto i = 0; i < 4; i++) {
+        p ^= -(b & 1) & a;
+        auto mask = -((a >> 3) & 1);
+        // 0b10011 is x^4 + x + 1
+        a = (a << 1) ^ (0b10011 & mask);
+        b >>= 1;
+    } 
+
+    return p;
+}
+
+std::unordered_map<uint8_t, uint8_t> MiniAES::s_box = {
+    {0b0000, 0b1110}, {0b1000, 0b0011},
+    {0b0001, 0b0100}, {0b1001, 0b1010},
+    {0b0010, 0b1101}, {0b1010, 0b0110},
+    {0b0011, 0b0001}, {0b1011, 0b1100},
+    {0b0100, 0b0010}, {0b1100, 0b0101},
+    {0b0101, 0b1111}, {0b1101, 0b1001},
+    {0b0110, 0b1011}, {0b1110, 0b0000},
+    {0b0111, 0b1000}, {0b1111, 0b0111}
+};
+
+matrix MiniAES::blockToArray(std::string s) {
+    // converts 16 bit string to 2x2 nibble array
+    // uses masks to extract high/low nibble (bit shift, &)
+    // smallest data type in C++ is 1 byte (8 bit) so uint8_t gives the most efficient output
+    matrix vals;
+    for (unsigned i = 0; i < 2; i++) {
+        vals[0][i] = s[i] >> 4; // high nibble
+        vals[1][i] = s[i] & 15; // low nibble
+    }
+    return vals;
+}
+
+matrix MiniAES::nibbleSub (matrix block) {
+    for (auto i = 0; i < block.max_size(); i++) {
+        for (auto j = 0; j < block[i].max_size(); j++)
+            block[i][j] = MiniAES::s_box[block[i][j]];
+    }
+
+    return block;
+}
+
+matrix MiniAES::shiftRow(matrix block) {
+    uint8_t temp = block[1][1];
+    block[1][1] = block[1][0];
+    block[1][0] = temp;
+    return block;
+}
+
+// matrix MiniAES::mixColumn(matrix block) {
+    
+// }
+
+std::string MiniAES::encrypt (std::string pt) { 
+    // block cipher, each block has 16 bits / 2 bytes
+    for (auto i = 0; i < pt.length(); i += 2) {
+        auto block = this->blockToArray(pt.substr(i, 2));
+        auto sub = this->nibbleSub(block);
+        auto shift = this->shiftRow(block);
+        auto test = gmul(15, 14);
+    }
+
+    return "hi";
+}
+
+ 
+int main() {
+    std::string pt ("hiwfqwef"); // plain text
+    std::string key (""); // secret key
+
+    // for (unsigned i = 0; i < pt.length(); i += 2) {
+    //     std::cout << pt.substr(i, 2) << std::endl;
+    // }
+
+    MiniAES test(key);
+    std::cout << test.encrypt(pt);
+
+}
---
+++ MiniAES.h
@@ -0,0 +1,26 @@
+#include <array>
+#include <string>
+#include <unordered_map>
+
+// 2 x 2 uint8_t array
+typedef std::array< std::array<uint8_t, 2>, 2 > matrix;
+
+class MiniAES {
+    private:
+        std::string key;
+    public:
+        static std::unordered_map<uint8_t, uint8_t> s_box;      // S-box implementation using hash map
+        MiniAES() {};
+        MiniAES(std::string k) : key(k) {};
+        void setKey (std::string k) {
+            key = k;
+        };
+        matrix blockToArray(std::string);
+        matrix nibbleSub(matrix);
+        matrix shiftRow(matrix);
+        matrix mixColumn(matrix);
+        std::string encrypt(std::string);
+};
+
+uint8_t gadd(uint8_t, uint8_t);
+uint8_t gmul(uint8_t, uint8_t);
